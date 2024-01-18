9196aa94709eef7c7996648d5a29eb341876c6e2
ABDELILAH ARAB <156742703+AbdelilahDev@users.noreply.github.com>
2024-01-18 20:46:43 +0100

    Add files via upload

---
+++ Global Classes/clsGlobal.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Win32;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Buisness;
+
+
+namespace DVLD.Classes
+{
+    internal static  class clsGlobal
+    {
+        public static clsUser CurrentUser;
+
+        public static bool RememberUsernameAndPassword(string Username, string Password)
+        {
+
+
+            try
+            {
+                //this will get the current project directory folder.
+                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+
+                // Define the path to the text file where you want to save the data
+                string filePath = currentDirectory + "\\data.txt";
+
+                //incase the username is empty, delete the file
+                if (Username=="" && File.Exists(filePath)) 
+                { 
+                     File.Delete(filePath);
+                    return true;
+
+                }
+
+                // concatonate username and passwrod withe seperator.
+                string dataToSave = Username + "#//#"+Password ;
+
+                // Create a StreamWriter to write to the file
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    // Write the data to the file
+                    writer.WriteLine(dataToSave);
+                   
+                  return true;
+                }
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show ($"An error occurred: {ex.Message}");
+                return false;
+            }
+
+        }
+
+        public static bool GetStoredCredential(ref string UseName,ref string Password)
+        {
+            ////this will get the stored username and password and will return true if found and false if not found.
+            //try
+            //{
+            //    //gets the current project's directory
+            //    string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+            //    // Path for the file that contains the credential.
+            //    string filePath  = currentDirectory + "\\data.txt";
+
+            //    // Check if the file exists before attempting to read it
+            //    if (File.Exists(filePath))
+            //    {
+            //        // Create a StreamReader to read from the file
+            //        using (StreamReader reader = new StreamReader(filePath))
+            //        {
+            //            // Read data line by line until the end of the file
+            //            string line;
+            //            while ((line = reader.ReadLine()) != null)
+            //            {
+            //                Console.WriteLine(line); // Output each line of data to the console
+            //                string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+
+            //                Username = result[0];
+            //                Password = result[1];
+            //            }
+            //            return true;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show ($"An error occurred: {ex.Message}");
+            //    return false;   
+            //}
+
+            // Specify the Registry key and path
+            // string keyPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\YourSoftware";
+            string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD Project";
+            string PassWord = "Password";
+            string UserName = "UserName";
+            
+
+
+            try
+            {
+                // Read the value from the Registry
+                string valueUserName = Registry.GetValue(keyPath, UserName, null) as string;
+
+                string valuePassord = Registry.GetValue(keyPath, PassWord, null) as string;
+
+
+                if (valuePassord != null&& valueUserName!=null)
+                {
+                    UseName = valueUserName;
+                    Password = valuePassord;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+           
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    
+    
+    }
+}
