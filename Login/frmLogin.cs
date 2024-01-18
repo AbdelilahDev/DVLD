15410ecd9fefde96e9404caf87185605def6ec1f
ABDELILAH ARAB <156742703+AbdelilahDev@users.noreply.github.com>
2024-01-18 20:48:13 +0100

    Add files via upload

---
+++ Login/frmLogin.cs
@@ -0,0 +1,120 @@
+﻿using DVLD.Classes;
+using DVLD_Buisness;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            clsUser user= clsUser.FindByUsernameAndPassword(txtUserName.Text.Trim(),txtPassword.Text.Trim());
+
+            if (user != null) 
+            { 
+
+                if (chkRememberMe.Checked )
+                {
+                    //store username and password
+                    clsGlobal.RememberUsernameAndPassword(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+                    // string keyPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\YourSoftware";
+                    string keyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD Project";
+
+                    string UserName = "UserName";
+                    string valueData = txtUserName.Text.Trim();
+
+                    string Password = "Password";
+                    string value = txtPassword.Text.Trim();
+
+
+                    try
+                    {
+                        // Write the value to the Registry
+                        Registry.SetValue(keyPath, UserName, valueData, RegistryValueKind.String);
+
+                        Registry.SetValue(keyPath, Password, value, RegistryValueKind.String);
+
+                       // Console.WriteLine($"Value {UserName} successfully written to the Registry.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+                } 
+                  else
+                {
+                    //store empty username and password
+                    clsGlobal.RememberUsernameAndPassword("", "");
+
+                }
+
+                //incase the user is not active
+                if (!user.IsActive )
+                {
+
+                    txtUserName.Focus();
+                    MessageBox.Show("Your accound is not Active, Contact Admin.", "In Active Account", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                 clsGlobal.CurrentUser = user;
+                 this.Hide();
+                 frmMain frm = new frmMain(this);
+                 frm.ShowDialog();
+
+
+            } 
+            else
+            {
+                txtUserName.Focus();
+                MessageBox.Show("Invalid Username/Password.", "Wrong Credintials", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }    
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            string UserName = "", Password = "";
+
+            if (clsGlobal.GetStoredCredential(ref UserName, ref Password))
+            {
+                txtUserName.Text = UserName;
+                txtPassword.Text = Password;
+                chkRememberMe.Checked = true;
+            }
+            else
+                chkRememberMe.Checked = false;
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtPassword_VisibleChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
